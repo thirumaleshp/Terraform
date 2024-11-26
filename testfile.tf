3a84d31c0ec5ca637f3060406d91ea7c1aa3a6d4
Thirumalesh <85149081+thirumaleshp@users.noreply.github.com>
2024-11-26 07:33:43 +0530

    Update testfile.tf

--- testfile.tf
+++ testfile.tf
@@ -1,1 +1,24 @@
 
+provider "google" {
+  project = "mygcptestproject-437406"
+  region  = "us-central1"
+}
+
+resource "google_compute_instance" "my_vm" {
+  name         = "my-tfvm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2404-noble-arm64-v20241115"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // One access config is required for external IP
+    }
+  }
+}
